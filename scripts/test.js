2eb0187255327e76b39f61396f3478b30126eb9d
Frank Paczynski <frank.paczynski@solygen.de>
2017-03-28 09:41:36 +0200

    throw error in test script

--- scripts/test.js
+++ scripts/test.js
@@ -4,20 +4,25 @@ var util = require('./util');
 
 var data = util.getExercises();
 var result = [];
+var error = false;
 
 cont('missing name', function (obj) { return !obj.name; });
 cont('missing/wrong type', function (obj) { return !/isometric|isolation|compound/.test(obj.type); }, function (obj) { return obj.id + (obj.type ? '(' + obj.type + ')' : ''); });
 cont('missing primary', function (obj) { return !obj.primary; });
 cont('missing secondary', function (obj) { return !obj.secondary; });
-cont('missing equipment', function (obj) { return !obj.equipment; });
+cont('!missing equipment', function (obj) { return !obj.equipment; });
 cont('missing steps', function (obj) { return !obj.steps; });
 
 // output
 _.each(result, function (data) {
     console.log('---');
+    if (data.name.indexOf('!') === 0) error = true;
     console.log(data.name);
     console.log(data.list.join(','));
 });
+
+
+if (error) throw new Error('Whoops!');
 
 function cont(name, fnFilter, fnMap) {
     var list = _.chain(data).filter(fnFilter).map(fnMap || function (data) {
